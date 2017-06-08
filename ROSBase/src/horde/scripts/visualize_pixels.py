79b7d6227e6f9092f404f0d819ae288dbf8f5a83
michelealbach <malbach@ualberta.ca>
2017-06-08 11:15:51 -0600

    commented

--- ROSBase/src/horde/scripts/visualize_pixels.py
+++ ROSBase/src/horde/scripts/visualize_pixels.py
@@ -1,23 +1,47 @@
-#!/usr/bin/env python
+"""
+Michele Albach
+June 8th 2017
+
+Visualizer:
+
+initializes by creating a voronoi diagram to match the image size
+and set of points
+
+then update_colours can be called with image data to properly 
+colorize the voronoi diagram
+
+updates an image called visualization.png which will update 
+automatically with each change (even when open)
+"""
 
 from PIL import Image
 import csv
 from tools import timing
 
 class Visualize():
+    # Note: creating the diagram takes some time, but should only
+    #       need to be done once when the random points are chosen
     def __init__(self,points,imsizex,imsizey):
 
+        # the diagram is saved as a dictionary
         self.voronoi={(p[0],p[1]):[] for p in points}
+
+        # for every pixel in the image:
         for x in range(imsizex):
             for y in range(imsizey):
+                # check every point to see if it's closer than the
+                #      closest so far
                 mini=imsizex*imsizex+imsizey*imsizey
                 for p in points:
                     dist = (p[0]-x)*(p[0]-x)+(p[1]-y)*(p[1]-y)
                     if dist < mini:
                         mini=dist
                         fp=p
+                # save the pixel into the value of the closest point
                 self.voronoi[(fp[0],fp[1])].append([x,y])
 
+    # update_colours: goes through each region in the voronoi diagram and
+    #      colours all pixels in that region the colour of the point
     @timing
     def update_colours(self,image):
     
@@ -25,30 +49,11 @@ class Visualize():
         im=Image.new("RGB",(len(image),len(image[0])))
         pix = im.load()
         
-        # Open and read CSV file
-        # f=open("rgb.csv","rb")
-        # colours=csv.reader(f)
-        
-        # Save read values to a matrix
-        # colours2=[[0 for x in range(3)] for y in range(3)]
-        # i=0
-        # for colour in colours:
-        #     colours2[i][0]=colour[0]
-        #     colours2[i][1]=colour[1]
-        #     colours2[i][2]=colour[2]
-        #    i=i+1
-        
-        # Assign the values to the image
-        # This part can be changed to rearrange what the image looks like.
-        
-        # set each pixel to be the colour of the closest point (voronoi)
-
+        # for each pixel in each region, colour it to match the region's point
         for r in self.voronoi:
             for p in self.voronoi[r]:
                 pix[p[0],p[1]]=(int(image[r[0]][r[1]][0]),int(image[r[0]][r[1]][1]),int(image[r[0]][r[1]][2]))
 
-
-        # f.close()
 
         # Save the image
         im.save("visualization.png", "PNG")
