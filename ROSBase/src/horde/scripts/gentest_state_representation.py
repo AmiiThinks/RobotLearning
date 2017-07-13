47118b9eec0839b1868ada70e44469d1ea484457
ParashRahman <parash@ualberta.ca>
2017-07-13 14:17:12 -0600

    removed some debugging statements. no bugs found in gen and test. fixes #23

--- ROSBase/src/horde/scripts/gentest_state_representation.py
+++ ROSBase/src/horde/scripts/gentest_state_representation.py
@@ -6,7 +6,7 @@ from state_representation import StateConstants, StateManager
 from tools import overrides
 
 class GenTestStateConstants:
-	MIN_ALIVE_TIME = 30
+	MIN_ALIVE_TIME = 100
 	NUM_POINTS_REPLACE = 60
 
 class GenTestStateManager(StateManager):
@@ -18,13 +18,7 @@ class GenTestStateManager(StateManager):
 	def get_state_representation(self, image, bumper_information, action, weights = None):
 		# update weights to reflect removal of features
 
-		rospy.loginfo("WEIGHTS")
-		rospy.loginfo(weights)
-
 		if (weights is not None):
-			rospy.loginfo("ENTERING DEBUG ZONE")
-			start_time = timeit.timeit()
-
 			pixel_weights = np.zeros(StateConstants.NUM_RANDOM_POINTS)
 
 			for w in range(StateConstants.NUM_RANDOM_POINTS):
@@ -48,14 +42,6 @@ class GenTestStateManager(StateManager):
 					for index in range(3 * StateConstants.NUM_FEATURES_PER_COL_VAL):
 						weights[3 * pixel * StateConstants.NUM_FEATURES_PER_COL_VAL + index] = 0
 			
-			rospy.loginfo("NUM LOWEST:")
-			rospy.loginfo(len(lowest_pixels))
-			rospy.loginfo("EXITING DEBUG ZONE")
-			rospy.loginfo("TIME TAKEN:")
-			
-			end_time = timeit.timeit()
-
-			rospy.loginfo(end_time - start_time)
 		# get the new state
 		feature_state = super(GenTestStateManager, self).get_state_representation(
 			image, bumper_information, action)
