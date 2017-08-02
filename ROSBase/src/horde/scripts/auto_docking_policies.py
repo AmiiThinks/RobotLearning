0ccb1eb7a3ea76eb05c8d88d830068772162b324
Shibhansh Dohare <shibhanshdohare1997@gmail.com>
2017-08-02 12:07:00 -0600

    add/forward if bump policy in auto-docking-policies

--- ROSBase/src/horde/scripts/auto_docking_policies.py
+++ ROSBase/src/horde/scripts/auto_docking_policies.py
@@ -57,10 +57,43 @@ class Alternating_Rotation(Policy):
             self.last_index = self.LEFT
         else: 
             self.last_index = self.RIGHT
-        print 'in alternating Rotation num time step is : ', self.time_steps
 
         self.pi = np.zeros(self.action_space.size, float)
         self.pi[self.last_index] = 1
 
         self.time_steps += 1
         self.time_steps %= 2*self.num_time_steps
+
+class ForwardIfClear(Policy):
+    def __init__(self, *args, **kwargs):
+        # where the last action is recorded according
+        # to its respective constants
+        # these indices should be in order of action space
+        self.STOP = 0
+        self.FORWARD = 1
+        self.TURN_RIGHT = 2
+        self.TURN_LEFT = 2
+        Policy.__init__(self, *args, **kwargs)
+
+    def update(self, phi, observation, *args, **kwargs):
+        if sum(observation['bump']):
+            random_number = random.uniform(0, 1)
+            print 'bumping....'
+            if random_number > 0.5:
+                self.last_index = self.TURN_RIGHT
+            else:
+                self.last_index = self.TURN_LEFT
+        else:
+            previous_index = self.last_index
+            random_number = random.uniform(0, 1)
+            if random_number > 0.95:
+                self.last_index = self.TURN_LEFT
+            elif random_number > 0.9:
+                self.last_index = self.TURN_RIGHT
+            else:
+                self.last_index = self.FORWARD
+            if (previous_index == self.TURN_RIGHT or previous_index == self.TURN_LEFT) and self.last_index == self.FORWARD:
+                self.last_index = self.STOP
+        # return 'behavior_policy'
+        self.pi = np.zeros(self.action_space.size)
+        self.pi[self.last_index] = 1
